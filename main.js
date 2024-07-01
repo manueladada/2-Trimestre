65e28778e61f8dc33f97109a6cd970ab774ea319
manueladada <161317765+manueladada@users.noreply.github.com>
2024-07-01 08:54:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const CaixaPrincipal = document.querySelector(".Caixa-Principal");
+const CaixaPerguntas = document.querySelector(".Caixa-Perguntas");
+const CaixaAlternativas = document.querySelector(".Caixa-Alternativas");
+const CaixaResultado = document.querySelector(".Caixa-Resultado");
+const TextoResultado = document.querySelector(".Texto-Resultado");
+
+const Perguntas = [
+    {
+    Enunciado: "Pergunta 1",
+    Alternativas: ["Alternativa 1", "Alternativa 2"]
+    },
+
+
+{
+     Enunciado: "Pergunta 2",
+Alternativas: ["Alternativa 1", "Alternativa 2"]
+},
+
+]
